b6f53d707316574c39b5d49e29f045f23ebff1be
ulianasysoeva <sysoeva@thehead.ru>
2023-01-27 15:57:09 +0500

    save changes

--- lib/src/ui/widgets/paginator_content/number_content.dart
+++ lib/src/ui/widgets/paginator_content/number_content.dart
@@ -26,18 +26,13 @@ class NumberContent extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              if (!_frontDotsShouldShow(context, availableSpots) && !_backDotsShouldShow(context, availableSpots))
-                SizedBox(width: 16,),
               _buildPageButton(context, 0),
-
               if (_frontDotsShouldShow(context, availableSpots))
                 _buildDots(context),
               if (InheritedNumberPaginator.of(context).numberPages > 1)
                 ..._generateButtonList(context, availableSpots),
               if (_backDotsShouldShow(context, availableSpots))
                 _buildDots(context),
-              if (!_frontDotsShouldShow(context, availableSpots) && !_backDotsShouldShow(context, availableSpots))
-                SizedBox(width: 16,),
               if (InheritedNumberPaginator.of(context).numberPages > 1)
                 _buildPageButton(context,
                     InheritedNumberPaginator.of(context).numberPages - 1),
